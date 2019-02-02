60142355cd3cf3ae5b114590f96b9f29b2a7bbe6
Jerick Bilalat <jrk.mb.xm@gmail.com>
2019-02-02 14:37:16 -0600

    fix base api url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 
 export const CLIENT_BASE_URL = "http://shop-minnesotarec.surge.sh";
-export const API_SERVER_BASE_URL = "https://minnesotarec.herokuapp.com/api/product";
+export const API_SERVER_BASE_URL = "https://minnesotarec.herokuapp.com";
 export const DEV_CLIENT_BASE_URL = "http://localhost:3000";
